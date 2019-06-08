44333be8652ae7bbb6ec39d2aee10612e3052f96
FredHerberts <51488709+FredHerberts@users.noreply.github.com>
2019-06-08 18:33:21 +0200

    Update project euler #4.py

--- project euler #4.py
+++ project euler #4.py
@@ -2,8 +2,8 @@ import time
 start = time.time()
 palindrome = []
 def maxi(x):
-    for x in range(900,x,1):
-        for y in range(900,x,1):
+    for x in range(900,x):
+        for y in range(900,x):
             if str(x*y)==str(x*y)[::-1]:
                 palindrome.append(x*y)
 maxi(1000)
